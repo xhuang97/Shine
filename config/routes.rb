46f14399f5b3e5c74fa6d4e9cbee7570726f1593
Killian Huang <xhuang1@andrew.cmu.edu>
2017-08-15 20:05:35 -0400

    fixing styles

--- config/routes.rb
+++ config/routes.rb
@@ -61,24 +61,32 @@ Rails.application.routes.draw do
   resources :registries
 
 
- 
+
   get 'home' => 'home#home', as: :home
   get 'about' => 'home#about', as: :about
   get 'contact' => 'home#contact', as: :contact
   get 'privacy' => 'home#privacy', as: :privacy
   get 'analytics' => 'home#analytics', as: :analytics
 
+  # get 'login' => 'sessions#new', :as => :login
+  # post 'sessions' => 'sessions#create'
+  # get 'logout' => 'sessions#destroy', :as => :logout
+
+  # login, sign up routes
+  resources :users
+  resources :sessions
+  get 'signup' => 'users#new', :as => :signup
   get 'login' => 'sessions#new', :as => :login
-  post 'sessions' => 'sessions#create'
   get 'logout' => 'sessions#destroy', :as => :logout
+
 
 
   get 'newsletter' => 'newsletter#new', :as => :newsletter
   post 'newsletter/create' => 'newsletter#create'
   get 'newsletter/create' => 'newsletter#new'
- 
-  
-  
+
+
+
   #get '*a', to: 'error#routing'
-  
+
 end
